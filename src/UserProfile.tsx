79b301d91c5b83f8ea036e1998fed1c4434c83c7
WFZ1 <pavelskumenko@gmail.com>
2024-05-04 10:20:16 +0300

    refactor: rewrite UserProfile component according to Liskov substitution principle

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -5,6 +5,11 @@ type UserDetailsResponse = {
     email: string;
     avatarUrl: string;
 };
+
+interface AdminDetailsResponse extends UserDetailsResponse {
+    isAdmin: boolean;
+}
+
 const fetchUserDetails = (userId: string): Promise<UserDetailsResponse> =>
     new Promise((resolve) => {
         setTimeout(
@@ -18,7 +23,19 @@ const fetchUserDetails = (userId: string): Promise<UserDetailsResponse> =>
         );
     });
 
-const Loader = () => 'loading...';
+const fetchAdminDetails = (adminId: string): Promise<AdminDetailsResponse> =>
+    new Promise((resolve) => {
+        setTimeout(
+            () =>
+                resolve({
+                    name: 'Cat',
+                    email: 'cat@mittens.meow',
+                    avatarUrl: 'https://placekitten.com/100/100',
+                    isAdmin: true,
+                }),
+            100
+        );
+    });
 
 const useUserDetails = (userId: string) => {
     const [userDetails, setUserDetails] = React.useState<UserDetailsResponse | null>(null);
@@ -30,6 +47,18 @@ const useUserDetails = (userId: string) => {
     return userDetails;
 };
 
+const useAdminDetails = (adminId: string) => {
+    const [adminDetails, setAdminDetails] = React.useState<AdminDetailsResponse | null>(null);
+
+    React.useEffect(() => {
+        fetchAdminDetails(adminId).then(setAdminDetails);
+    }, [adminId]);
+
+    return adminDetails;
+};
+
+const Loader = () => 'loading...';
+
 const UserDetails = ({ userDetails }: { userDetails: UserDetailsResponse }) => {
     return (
         <>
@@ -39,38 +68,64 @@ const UserDetails = ({ userDetails }: { userDetails: UserDetailsResponse }) => {
         </>
     );
 };
-const UserProfileLayout = ({ children }: React.PropsWithChildren) => {
+
+const AdminDetails = ({ adminDetails }: { adminDetails: AdminDetailsResponse }) => {
+    return (
+        <>
+            <h2>{adminDetails.name}</h2>
+            <p>{adminDetails.email}</p>
+            <img src={adminDetails.avatarUrl} alt="User Avatar" />
+            {adminDetails.isAdmin && <p>I am admin</p>}
+        </>
+    );
+};
+
+interface ProfileProps<T> {
+    details: T | null;
+    LoaderComponent?: React.FunctionComponent;
+    renderDetails?: (details: T) => JSX.Element;
+}
+
+const Profile = <T extends UserDetailsResponse>({
+    details,
+    LoaderComponent = Loader,
+    renderDetails = (details: UserDetailsResponse) => <UserDetails userDetails={details} />,
+}: ProfileProps<T>) => {
+    return details ? renderDetails(details) : <LoaderComponent />;
+};
+
+const ProfileLayout = ({ children }: React.PropsWithChildren) => {
     return <div>{children}</div>;
 };
 
-interface UserProfileProps {
-    userDetails: UserDetailsResponse | null;
-    LoaderComponent?: React.FunctionComponent;
-    renderUserDetails?: (userDetails: UserDetailsResponse) => JSX.Element;
+interface SelfLoadingProfileProps<T> {
+    id: string;
+    renderDetails?: (details: NonNullable<T>) => JSX.Element;
 }
 
-const UserProfile = ({
-    userDetails,
-    LoaderComponent = Loader,
-    renderUserDetails = (details: UserDetailsResponse) => <UserDetails userDetails={details} />,
-}: UserProfileProps) => {
-    return userDetails ? renderUserDetails(userDetails) : <LoaderComponent />;
-};
+const getSelfLoadingProfile =
+    <T extends UserDetailsResponse | null>(useDetails: (id: string) => T) =>
+    ({ id, renderDetails }: SelfLoadingProfileProps<T>) => {
+        const details = useDetails(id);
 
-const SelfLoadingUserProfile = ({ userId }: { userId: string }) => {
-    const userDetails = useUserDetails(userId);
+        return (
+            <ProfileLayout>
+                <Profile details={details} renderDetails={renderDetails} />
+            </ProfileLayout>
+        );
+    };
 
-    return (
-        <UserProfileLayout>
-            <UserProfile userDetails={userDetails} />
-        </UserProfileLayout>
-    );
-};
+const SelfLoadingUserProfile = getSelfLoadingProfile(useUserDetails);
+const SelfLoadingAdminProfile = getSelfLoadingProfile(useAdminDetails);
 
 export default function App() {
     return (
         <div className="App">
-            <SelfLoadingUserProfile userId="123" />
+            <SelfLoadingUserProfile id="123" />
+            <SelfLoadingAdminProfile
+                id="321"
+                renderDetails={(details: AdminDetailsResponse) => <AdminDetails adminDetails={details} />}
+            />
         </div>
     );
 }
